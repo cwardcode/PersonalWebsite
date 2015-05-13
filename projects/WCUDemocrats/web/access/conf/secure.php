dac5e43993df13abaa27894401843ded9a65701b
Chris Ward <chris@cwardcode.com>
2015-05-13 19:15:16 -0400

    Removed unnecessary files

--- projects/WCUDemocrats/web/access/conf/secure.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $data_db="cdward4_demotest";
-    $user_db="root";
-    $pass_db="B33awNfj";
-    $host_db="localhost";
-?> 
--- projects/WCUDemocrats/web/access/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['user']);
-    echo '<META HTTP-EQUIV="Refresh" Content="0; URL=index.php">';    
-    exit;
-?>
